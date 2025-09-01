804d655f845189c43e09ab53e2b0964fe14db8bb
SpadeS <calebathoren@gmail.com>
2025-09-01 12:52:59 -0700

    Merge branch 'ZombieSurvival-V1.5' of https://github.com/calebthoren/ZombieSurvival into ZombieSurvival-V1.5

--- systems/resourceSystem.js
+++ systems/resourceSystem.js
@@ -665,10 +665,12 @@ function createResourceSystem(scene) {
             const baseTex = scene.textures.get(
                 baseDef.world?.textureKey || baseId,
             );
-            const src = baseTex.getSourceImage();
-            const scale = baseDef.world?.scale ?? 1;
-            const width = src.width * scale;
-            const height = src.height * scale;
+          
+            // derive width/height from base variant for spacing
+            const baseSrc = baseTex.getSourceImage();
+            const baseScale = baseDef.world?.scale ?? 1;
+            const width = baseSrc.width * baseScale;
+            const height = baseSrc.height * baseScale;
 
             const firstId = baseId;
             const firstDef = RESOURCE_DB[firstId];
