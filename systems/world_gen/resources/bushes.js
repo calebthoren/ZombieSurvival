66e1429fd152a855da5719c612a111c0d56810a4
SpadeS <54781608+calebthoren@users.noreply.github.com>
2025-09-14 15:49:00 -0700

    chore(resources): consolidate resource registration
    ZombieSurvival v1.6.7
    
    Features:
    - none
    
    Bugfixes:
    - none
    
    Other:
    - Consolidate rocks, trees, and bushes registration into systems/world_gen/resources/index.js.
    - Replace individual resource imports with the new index module.
    - Remove obsolete resource registration files.

--- systems/world_gen/resources/bushes.js
+++
@@ -1,4 +0,0 @@
-import { WORLD_GEN } from '../worldGenConfig.js';
-import { registerResourceType } from './registry.js';
-
-registerResourceType('bushes', () => WORLD_GEN?.spawns?.resources?.bushes);
